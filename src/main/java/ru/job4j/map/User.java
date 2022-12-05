ea77c86fd39276c1eb8f56afea5da4c8f1f28196
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2022-12-05 19:17:06 +0300

    2. Добавить метод boolean isBinary() [#1712]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -14,27 +14,6 @@ public class User {
     }
 
     public static void main(String[] args) {
-        /**2. Без переопределения equals и hashCode [#1005 #365966]
-        Calendar birthday = new GregorianCalendar(2000, 7, 17);
-        User user1 = new User("Alex", 2, birthday.getTime());
-        User user2 = new User("Alex", 2, birthday.getTime());
-        Map<User, Object> map = new HashMap<>();
-        map.put(user1, new Object());
-        map.put(user2, new Object());
-        System.out.println(user1);
-        System.out.println(user2);
-        System.out.println("Map" + map);
-        int hashCode1 = user1.hashCode();
-        int hash1 = hashCode1 ^ (hashCode1 >>> 16);
-        int bucket1 = hash1 & 15;
-        System.out.printf("user1 - хэшкод: %s, хэш: %s, бакет: %s", hashCode1, hash1, bucket1);
-        System.out.println();
-        int hashCode2 = user2.hashCode();
-        int hash2 = hashCode2 ^ (hashCode2 >>> 16);
-        int bucket2 = hash2 & 15;
-        System.out.printf("user2 - хэшкод: %s, хэш: %s, бакет: %s", hashCode2, hash2, bucket2);
-         */
-        //3. Переопределить только hashCode [#1003]
         Calendar birthday = new GregorianCalendar(2000, Calendar.APRIL, 17, 11, 5, 0);
         User user1 = new User("Alex", 2, 5);
         User user2 = new User("Alex", 2, 5);
@@ -64,8 +43,12 @@ public class User {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         User user = (User) o;
         return children == user.children && birthday == user.birthday && name.equals(user.name);
     }
