5e768efac52e36044fc60cbc00febed1ce8c47b5
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-01-31 09:03:59 +0300

    10. Сканирование файловой системы [#106929]

--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -13,8 +13,8 @@ import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class SearchFiles implements FileVisitor<Path> {
 
-    Predicate<Path> condition;
-    List<Path> fileList = new ArrayList<>();
+    private Predicate<Path> condition;
+    private List<Path> fileList = new ArrayList<>();
 
     public SearchFiles(Predicate<Path> condition) {
         this.condition = condition;
