8cec7ba69f23ff9e713d682de1f4d5e0def23587
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-02-08 23:33:29 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,10 +8,22 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
 
+
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".class")).forEach(System.out::println);
+        Path start = Paths.get("C:/Users/Ivan_Kozhevnikov/IdeaProjects/job4j_design/data");
+        validate(start);
+        search(start, p -> p.toFile().getName().endsWith(".txt")).forEach(System.out :: println);
+    }
+
+    private static void validate(Path start) throws IOException {
+        File file = new File("data");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Folder not exist: %s", file.getAbsoluteFile()));
+        }
+        if (search(start, p -> p.toFile().getName().endsWith(".txt")).isEmpty()) {
+            throw new IllegalArgumentException("Not file .txt");
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
