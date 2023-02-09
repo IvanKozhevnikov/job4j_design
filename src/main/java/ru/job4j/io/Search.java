c7977fd0c3be597727a49105c44897fce0158451
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-02-10 01:26:05 +0300

    12. Валидация параметров запуска [#246865 #389839]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,6 +1,5 @@
 package ru.job4j.io;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,18 +10,18 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("C:/Users/Ivan_Kozhevnikov/IdeaProjects/job4j_design/data");
-        validate(start);
-        search(start, p -> p.toFile().getName().endsWith(".txt")).forEach(System.out :: println);
+        validate(args);
+        Path start = Paths.get(args[0]);
+        System.out.println(start);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
-    private static void validate(Path start) throws IOException {
-        File file = new File("data");
-        if (!file.exists()) {
-            throw new IllegalArgumentException(String.format("Folder not exist: %s", file.getAbsoluteFile()));
+    private static void validate(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("The path to the folder is not specified");
         }
-        if (search(start, p -> p.toFile().getName().endsWith(".txt")).isEmpty()) {
-            throw new IllegalArgumentException("Not file .txt");
+        if (args.length == 1) {
+            throw new IllegalArgumentException("File extension not specified");
         }
     }
 
