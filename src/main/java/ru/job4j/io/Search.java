24f20cf544ad2486596b87a05985a294d1769332
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-02-14 02:06:03 +0300

    12. Валидация параметров запуска [#246865 #389839]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,22 +13,29 @@ public class Search {
     public static void main(String[] args) throws IOException {
         validate(args);
         Path start = Paths.get(args[0]);
-        System.out.println(start);
         search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
-    private static void validate(String[] args) {
+    private static void validate(String[] args) throws IOException {
         if (args.length == 0) {
-            throw new IllegalArgumentException("The path to the folder is not specified");
+            throw new IllegalArgumentException("The launch requires to specify the first parameter and the second parameter");
         }
         if (args.length == 1) {
-            throw new IllegalArgumentException("File extension not specified");
+            throw new IllegalArgumentException("The launch requires to specify the second parameter");
+        }
+        File file = new File(args[0]);
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("No such directory %s", file.getAbsoluteFile()));
+        }
+        Path start = Paths.get(args[0]);
+        if (search(start, p -> p.toFile().getName().endsWith(args[1])).isEmpty()) {
+            throw new IllegalArgumentException(String.format("There are no files with this extension %s", args[1]));
         }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
-        SearchFiles searcher = new SearchFiles(condition);
-        Files.walkFileTree(root, searcher);
-        return searcher.getPaths();
+        SearchFiles searchFiles = new SearchFiles(condition);
+        Files.walkFileTree(root, searchFiles);
+        return searchFiles.getPaths();
     }
 }
