6a98d9a8ce408e84d407165f4c4c961441dc4f59
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-03-12 19:20:03 +0300

    17

--- src/test/java/ru/job4j/set/SimpleSetTest.java
+++ src/test/java/ru/job4j/set/SimpleSetTest.java
@@ -1,47 +1,47 @@
-//package ru.job4j.set;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.Iterator;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//class SimpleSetTest {
-//
-//    @Test
-//    void whenAddNonNull() {
-//        Set<Integer> set = new SimpleSet<>();
-//        assertThat(set.add(1)).isTrue();
-//        assertThat(set.contains(1)).isTrue();
-//        assertThat(set.add(1)).isFalse();
-//    }
-//
-//    @Test
-//    void whenAddNull() {
-//        Set<Integer> set = new SimpleSet<>();
-//        assertThat(set.add(null)).isTrue();
-//        assertThat(set.contains(null)).isTrue();
-//        assertThat(set.add(null)).isFalse();
-//    }
-//
-//    @Test
-//    void whenHaveElementAndWhenHaveNotElement() {
-//        Set<Integer> set = new SimpleSet<>();
-//        set.add(7);
-//        Iterator<Integer> it = set.iterator();
-//        assertThat(it.hasNext()).isTrue();
-//        assertThat(it.next()).isEqualTo(7);
-//        assertThat(it.hasNext()).isFalse();
-//    }
-//    @Test
-//    void  whenGetIteratorThrice() {
-//        Set<Integer> set = new SimpleSet<>();
-//        set.add(7);
-//        set.add(9);
-//        set.add(null);
-//        Iterator<Integer> it = set.iterator();
-//        assertThat(it.next()).isEqualTo(7);
-//        assertThat(it.next()).isEqualTo(9);
-//        assertThat(it.next()).isEqualTo(null);
-//    }
-//}
+package ru.job4j.set;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SimpleSetTest {
+
+    @Test
+    void whenAddNonNull() {
+        Set<Integer> set = new SimpleSet<>();
+        assertThat(set.add(1)).isTrue();
+        assertThat(set.contains(1)).isTrue();
+        assertThat(set.add(1)).isFalse();
+    }
+
+    @Test
+    void whenAddNull() {
+        Set<Integer> set = new SimpleSet<>();
+        assertThat(set.add(null)).isTrue();
+        assertThat(set.contains(null)).isTrue();
+        assertThat(set.add(null)).isFalse();
+    }
+
+    @Test
+    void whenHaveElementAndWhenHaveNotElement() {
+        Set<Integer> set = new SimpleSet<>();
+        set.add(7);
+        Iterator<Integer> it = set.iterator();
+        assertThat(it.hasNext()).isTrue();
+        assertThat(it.next()).isEqualTo(7);
+        assertThat(it.hasNext()).isFalse();
+    }
+    @Test
+    void  whenGetIteratorThrice() {
+        Set<Integer> set = new SimpleSet<>();
+        set.add(7);
+        set.add(9);
+        set.add(null);
+        Iterator<Integer> it = set.iterator();
+        assertThat(it.next()).isEqualTo(7);
+        assertThat(it.next()).isEqualTo(9);
+        assertThat(it.next()).isEqualTo(null);
+    }
+}
