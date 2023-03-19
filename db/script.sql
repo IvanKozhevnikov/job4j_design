8f2da684ddbaa8f11f1df474b5422475d8f69568
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-03-19 15:40:20 +0300

    0. Что такое база данных. [#6860]

---
+++ db/script.sql
@@ -0,0 +1,14 @@
+create table shop_emploee_doc(
+id serial primary key,
+emploee varchar(255),
+salary integer,
+resume text,
+snils boolean);
+insert into shop_emploee_doc values (1, 'cashier', 30000,
+'the composition of the resume...', TRUE);
+update shop_emploee_doc set emploee = 'administrator',
+salary = 70000,
+resume = 'the composition of the resume...',
+snils = TRUE;
+delete from shop_emploee_doc
+where emploee = 'administrator';
