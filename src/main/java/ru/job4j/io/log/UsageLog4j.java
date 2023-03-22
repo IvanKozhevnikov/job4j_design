3ebdb235358adfbf514770af46f2f461d95522ea
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-03-22 11:15:34 +0300

    3. Slf4j - вывод переменных. [#268852 #403086]

--- src/main/java/ru/job4j/io/log/UsageLog4j.java
+++ src/main/java/ru/job4j/io/log/UsageLog4j.java
@@ -10,13 +10,14 @@ public class UsageLog4j {
     public static void main(String[] args) {
         String name = "Ivan";
         byte age = 5;
-        long inn = 98723496;
+        long inn = 98723496L;
         double weight = 90.5;
         int snils = 47685376;
-        float income = (float) 300000000000000.0;
+        float income = 300000000000000.0F;
         short languages = 2;
         boolean married = true;
-        LOG.debug("User info name : {}, age : {}, inn : {}, weight {}, snils {}, income {}, languages {}, married {}",
-                name, age, inn, weight, snils, income, languages, married);
+        char rights = 'B';
+        LOG.debug("User info name : {}, age : {}, inn : {}, weight {}, snils {}, income {}, languages {}, married {}, rights {}",
+                name, age, inn, weight, snils, income, languages, married, rights);
     }
 }
