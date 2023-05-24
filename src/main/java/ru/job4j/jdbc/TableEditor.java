31322fa06a2906cf5d8e4538778db2cf27ad8080
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-05-25 01:04:08 +0300

    0.1. Statement [#379306 #418180]

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -1,103 +1,100 @@
 package ru.job4j.jdbc;
 
-import junit.framework.Test;
-import org.apache.log4j.lf5.util.ResourceUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import ru.job4j.io.Config;
-
-
-import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectInputFilter;
-import java.net.URL;
-import java.nio.file.Files;
 import java.sql.*;
 import java.util.Properties;
 import java.util.StringJoiner;
 
 public class TableEditor implements AutoCloseable {
 
-    private static final Logger LOG = LoggerFactory.getLogger(TableEditor.class.getName());
+    private static Connection connection;
+    private static Properties properties;
 
-    private Connection connection;
-
-    private Properties properties;
-    private static Properties config = new Properties();
-
-    public TableEditor(Properties properties) {
+    public TableEditor(Properties properties) throws Exception {
         this.properties = properties;
         initConnection();
     }
 
-    public static void properties() throws IOException {
-
-        InputStream in = TableEditor.class.getClassLoader().getResourceAsStream("/app.properties");
-
-        System.out.println(TableEditor.class.getClassLoader().getResourceAsStream("/app.properties"));
-        try (in) {
-            System.out.println(in);
-            System.out.println(config.getProperty("url"));
-            config.load(in);
+    private void initConnection() throws Exception {
+        properties = new Properties();
+        try (InputStream in = TableEditor.class.getClassLoader().getResourceAsStream("app.properties")) {
+            properties.load(in);
         }
+        Class.forName(properties.getProperty("hibernate.connection.driver_class"));
+        String url = properties.getProperty("hibernate.connection.url");
+        String login = properties.getProperty("hibernate.connection.username");
+        String password = properties.getProperty("hibernate.connection.password");
+        connection = DriverManager.getConnection(url, login, password);
     }
 
-    private static Connection getConnection() throws Exception {
-        System.out.println(config.getProperty("username"));
-        String url = config.getProperty("url");
-        String login = config.getProperty("username");
-        String password = config.getProperty("password");
-        return DriverManager.getConnection(url, login, password);
-    }
-
-    private void initConnection() {
-        connection = null;
-    }
     // создает пустую таблицу без столбцов с указанным именем
     public void createTable(String tableName) throws Exception {
-        try (Connection connection = getConnection()) {
-            try (Statement statement = connection.createStatement()) {
-                String createTable = "CREATE TABLE IF NOT EXISTS " + tableName + "(%s, %s)";
-                statement.execute(createTable);
-            }
-        }
-
+                String createTable =  String.format(
+                        "CREATE TABLE IF NOT EXISTS %s();",
+                        tableName);
+                execute(createTable);
     }
     // удаляет таблицу по указанному имени
-    public void dropTable(String tableName) {
+    public void dropTable(String tableName) throws Exception {
+        String dropTable =  String.format(
+                "DROP TABLE IF EXISTS %s;",
+                tableName
+        );
+        execute(dropTable);
     }
     // добавляет столбец в таблицу
-    public void addColumn(String tableName, String columnName, String type) {
+    public void addColumn(String tableName, String columnName, String type) throws Exception {
+            String addColumn =  String.format(
+                    "ALTER TABLE %s ADD COLUMN %s %s;",
+                    tableName,
+                    columnName,
+                    type
+            );
+            execute(addColumn);
     }
     // удаляет столбец из таблицы
-    public void dropColumn(String tableName, String columnName) {
+    public void dropColumn(String tableName, String columnName) throws Exception {
+        String dropColumn =  String.format(
+                "ALTER TABLE %s DROP COLUMN %s;",
+                tableName,
+                columnName
+        );
+        execute(dropColumn);
     }
     // переименовывает столбец
-    public void renameColumn(String tableName, String columnName, String newColumnName) {
+    public void renameColumn(String tableName, String columnName, String newColumnName) throws Exception {
+        String renameColumn =  String.format(
+                "ALTER TABLE %s RENAME COLUMN %s TO %s;",
+                tableName,
+                columnName,
+                newColumnName
+        );
+        execute(renameColumn);
     }
-
-
 
     public String getTableScheme(String tableName) throws Exception {
         var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
         var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
         var buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
         buffer.add(header);
-        try (var statement = connection.createStatement()) {
-            var selection = statement.executeQuery(String.format(
-                    "SELECT * FROM %s LIMIT 1", tableName
-            ));
-            var metaData = selection.getMetaData();
-            for (int i = 1; i <= metaData.getColumnCount(); i++) {
-                buffer.add(String.format("%-15s|%-15s%n",
-                        metaData.getColumnName(i), metaData.getColumnTypeName(i))
-                );
+            try (var statement = connection.createStatement()) {
+                var selection = statement.executeQuery(String.format(
+                        "SELECT * FROM %s LIMIT 1", tableName
+                ));
+                var metaData = selection.getMetaData();
+                for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                    buffer.add(String.format("%-15s|%-15s%n",
+                            metaData.getColumnName(i), metaData.getColumnTypeName(i))
+                    );
+                }
             }
-        }
         return buffer.toString();
     }
 
+    private void execute(String sql) throws Exception {
+        Statement statement = connection.createStatement();
+        statement.execute(sql);
+    }
 
     @Override
     public void close() throws Exception {
@@ -107,15 +104,16 @@ public class TableEditor implements AutoCloseable {
     }
 
     public static void main(String[] args) throws Exception {
-        InputStream in = TableEditor.class.getClassLoader().getResourceAsStream("/app.properties");
-//        TableEditor.properties();
-        System.out.println(in);
-        Properties config = new Properties();
-        String tableName = "demo_table";
-
-                TableEditor tableEditor = new TableEditor(config);
-                tableEditor.createTable(tableName);
-                System.out.println(tableEditor.getTableScheme(tableName));
-
+        TableEditor tableEditor = new TableEditor(new Properties());
+        tableEditor.createTable("test_table");
+        System.out.println("1. Create table:\n" + tableEditor.getTableScheme("test_table"));
+        tableEditor.addColumn("test_table", "age", "int");
+        System.out.println("2. Add column:\n" + tableEditor.getTableScheme("test_table"));
+        tableEditor.renameColumn("test_table", "age", "new_column");
+        System.out.println("3. Rename column:\n" + tableEditor.getTableScheme("test_table"));
+        tableEditor.dropColumn("test_table", "new_column");
+        System.out.println("4. Drop column:\n" + tableEditor.getTableScheme("test_table"));
+        tableEditor.dropTable("test_table");
+        System.out.println("5. Drop table");
     }
 }
